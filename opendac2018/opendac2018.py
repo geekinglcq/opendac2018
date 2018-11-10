2a028ef02976fc2d141bf5167d78bc3fc2bca41f
legend <Legend94rz@gmail.com>
2018-11-10 22:22:00 +0800

    jupyter notebook

--- opendac2018/opendac2018.py
+++ opendac2018/opendac2018.py
@@ -1,1 +1,18 @@
+import json
+
+
+assignment_train = './data/assignment_train.json'
+pubs_train = './data/pubs_train.json'
+
+
+##
+# train: 'author_name' -> [ paper1, paper2, ... ]
+# cluster: 'author_name' -> [ [id1, id2,...], [id1, id2,...], ... ]
+# there are 100 authors needed to be disambiguated, ~24W appearing names, 147676 papers
+# each author in training set has 318.22 clusters in average.
+
+if __name__=="__main__":
+    cluster = json.load(open(assigment_train,'r'))
+    train_paper = json.load(open(ppubs_train,'r'))
+
 
