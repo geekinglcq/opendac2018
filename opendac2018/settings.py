96bf8edaf0a34f0a6c9cc043228cc89853de4f9f
lunar <lcqgeek@live.com>
2018-11-20 20:14:59 +0800

    :wrench: add path in settings

--- opendac2018/settings.py
+++ opendac2018/settings.py
@@ -10,17 +10,20 @@ VAL_NAME2PUB = join(DATA_DIR, 'await_validation.json')
 VAL_PATH = join(DATA_DIR, 'pubs_validate.json')
 IDF_THRESHOLD = 32
 cuda_visible_devices = '1'
-local_output_path       = join(OUTPUT_DIR, 'local_output.pkl')  # doc_id  -> Z_i, np.ndarray
+local_output_path = join(OUTPUT_DIR,
+                         'local_output.pkl')  # doc_id  -> Z_i, np.ndarray
 
 # Global settings
-assignments_train_path  = join(DATA_DIR, 'assignment_train.json')
-pubs_train_path         = './data/pubs_train.json'
-pubs_validate_path      = join(DATA_DIR, 'pubs_validate.json')
-stop_words_path         = './data/stop_words.txt'
-idf_path                = join(OUTPUT_DIR, 'idf.pkl')  # word    -> idf value, float
-global_output_path      = join(OUTPUT_DIR, 'global_output.pkl')  # doc_id  -> Y_i, np.ndarray
-material_path           = join(OUTPUT_DIR, 'material.pkl')  # doc_id  -> [word1, word2, ...], list
+assignments_train_path = join(DATA_DIR, 'assignment_train.json')
+pubs_train_path = join(DATA_DIR, './data/pubs_train.json')
+pubs_validate_path = join(DATA_DIR, 'pubs_validate.json')
+stop_words_path = './data/stop_words.txt'
+idf_path = join(OUTPUT_DIR, 'idf.pkl')  # word    -> idf value, float
+global_output_path = join(OUTPUT_DIR,
+                          'global_output.pkl')  # doc_id  -> Y_i, np.ndarray
+material_path = join(OUTPUT_DIR,
+                     'material.pkl')  # doc_id  -> [word1, word2, ...], list
 weighted_embedding_path = './output/weighted_embedding.pkl'  # doc_id  -> X_i, np.ndarray
-word2vect_model_path    = './output/word.emb'                   # word2vec model.  usage: Word2Vec.load(...)
-triple_set              = './output/triple.pkl'                           # 'emb'   -> anchors; 'emb_pos': positive weighted embedding; 'emb_neg': negative ones
-
+word2vect_model_path = './output/word.emb'  # word2vec model.  usage: Word2Vec.load(...)
+triple_set = './output/triple.pkl'  # 'emb'   -> anchors; 'emb_pos': positive weighted embedding; 'emb_neg': negative ones
+pos_pair_path = join(OUTPUT_DIR, 'pos_pair.json')
