d8c24a2a248716d7bcf4d1f2cd2d90ac43c6aa08
lunar <lcqgeek@live.com>
2018-11-27 09:41:29 +0800

    :sparkles: add PCKMeans

---
+++ opendac2018/active_semi_clustering/exceptions.py
@@ -0,0 +1,14 @@
+class ClusteringNotFoundException(Exception):
+    pass
+
+
+class EmptyClustersException(Exception):
+    pass
+
+
+class InconsistentConstraintsException(Exception):
+    pass
+
+
+class ShitException(Exception):
+    pass
